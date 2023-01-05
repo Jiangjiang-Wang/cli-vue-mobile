bed73d1fc5affbe7450e67fa5f580de74103df50
Zhijiang <zhijiang9190@qq.com>
2023-01-05 19:31:25 +0800

    feat: 1

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
  * @Author: wzj
  * @Date: 2023-01-05 18:38:39
  * @LastEditors: wzj
- * @LastEditTime: 2023-01-05 18:59:21
+ * @LastEditTime: 2023-01-05 19:31:05
 -->
 
 <template>
-    <div class="content">11111</div>
+    <div class="content">22222</div>
 </template>
 <script setup></script>
 
