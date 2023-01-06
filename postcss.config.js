8e586dc5055944e6c133945456b22b7b5c992ad6
Zhijiang <zhijiang9190@qq.com>
2023-01-06 10:16:23 +0800

    feat: 修改配置文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,3 @@
-/*
- * @FileDescription:
- * @Author: wzj
- * @Date: 2023-01-05 19:01:40
- * @LastEditors: wzj
- * @LastEditTime: 2023-01-06 09:38:52
- */
 module.exports = {
   plugins: {
     'postcss-px-to-viewport': {
